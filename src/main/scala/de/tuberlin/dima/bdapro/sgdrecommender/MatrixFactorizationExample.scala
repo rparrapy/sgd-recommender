4dc9c6cb0533c8640472bc589879d48315dbe73c
Duy Pham <pham.duy89@gmail.com>
2017-02-24 16:36:42 +0100

    joining multiple output ds into one

--- src/main/scala/de/tuberlin/dima/bdapro/sgdrecommender/MatrixFactorizationExample.scala
+++ src/main/scala/de/tuberlin/dima/bdapro/sgdrecommender/MatrixFactorizationExample.scala
@@ -13,8 +13,12 @@ import org.apache.flink.ml.optimization.SquaredLoss
   */
 object MatrixFactorizationExample extends App{
 
-  val HOME_PDUY = "/home/duy/"
+  val HOME_PDUY = "/home/duy/TUB/"
   val HOME_ROD = "/home/rparra/"
+  val YAHOO_ARTIST_TRAIN= "yahoo-artist-train.txt"
+  val YAHOO_ARTIST_TEST = "yahoo-artist-test.txt"
+  val YAHOO_SONG_TRAIN = "yahoo-song-train.txt"
+  val YAHOO_SONG_TEST = "yahoo-song-test.txt"
 
   val toLabeledVector = { (t: (Double, Double, Double, Double)) =>
     val features = t match {
@@ -26,41 +30,74 @@ object MatrixFactorizationExample extends App{
 
   val env  = ExecutionEnvironment.getExecutionEnvironment
 
+//  val train = env.readCsvFile[(Int, Int, Double, Double)](HOME_PDUY + YAHOO_ARTIST_NAME, fieldDelimiter = "\t")
   val train = env.readCsvFile[(Int, Int, Double, Double)]("/home/duy/TUB/1_semester/BDAPRO/ml-100k/u1.base", fieldDelimiter = "\t")
   val test = env.readCsvFile[(Int, Int, Double, Double)]("/home/duy/TUB/1_semester/BDAPRO/ml-100k/u1.test", fieldDelimiter = "\t")
 
 
   val trainData = train.map(x => (x._1, x._2, x._3))
+  val trainLabel = train.map(x => (x._1 + "-" + x._2, x._3))
   val testData = test.map(x => (x._1, x._2))
-  val testLabel= test.map(x => (x._1 + "-" + x._2, x._3))
+  val testLabel = test.map(x => (x._1 + "-" + x._2, x._3))
 
-  val lossFunction = GenericLossFunction(SquaredLoss, RecommenderPrediction)
+  val trainSize = trainData.collect.size
+  val testSize = testData.collect.size
 
-  val iteration = 200
-  val begin = System.nanoTime()
-  val sgd = SGDforMatrixFactorization()
-    .setIterations(iteration)
-    .setLambda(0.01)
-    .setBlocks(16)
-    .setNumFactors(40)
-    .setLearningRate(0.001) .setSeed(43L)
+//  val lossFunction = GenericLossFunction(SquaredLoss, RecommenderPrediction)
 
-  sgd.fit(trainData)
+//  val begin = System.nanoTime()
 
-  val testPredictions = sgd.predict(testData)
+  val resultDS = (1 to 5).map({
+    iteration => {
+      val sgd = SGDforMatrixFactorization()
+        .setIterations(iteration)
+        .setLambda(0.01)
+        .setBlocks(16)
+        .setNumFactors(40)
+        .setLearningRate(0.001).setSeed(43L)
 
-  testPredictions.map(x => (x._1 + "-" + x._2, x._3))
-    .join(testLabel)
-    .where(0)
-    .equalTo(0)
-    .map(x => (2 * SquaredLoss.loss(x._1._2, x._2._2) / 20000, 1))
-    .sum(0)
-    .map(x => ("Test MSE", x._1))
-    .writeAsCsv(HOME_PDUY + "output.txt", "\n", "\t", WriteMode.OVERWRITE)
+      sgd.fit(trainData)
+      val trainPredictions = sgd.predict(trainData.map(x => (x._1, x._2)))
+      val testPredictions = sgd.predict(testData)
+
+      val trainMSE = trainPredictions.map(x => (x._1 + "-" + x._2, x._3))
+        .join(trainLabel)
+        .where(0)
+        .equalTo(0)
+        .map(x => (2 * SquaredLoss.loss(x._1._2, x._2._2) / trainSize, 1))
+        .sum(0)
+        .map(x => ("Train MSE", x._1))
+
+      val testMSE = testPredictions.map(x => (x._1 + "-" + x._2, x._3))
+        .join(testLabel)
+        .where(0)
+        .equalTo(0)
+        .map(x => (2 * SquaredLoss.loss(x._1._2, x._2._2) / testSize, 1))
+        .sum(0)
+        .map(x => ("Test MSE", x._1))
+
+      (trainMSE, testMSE)
+    }
+  })
+
+  val finalResult = resultDS.reduce((left, right) => {
+    val leftTrain = left._1
+    val leftTest = left._2
+    val rightTrain = right._1
+    val rightTest =  right._2
+
+    (leftTrain.union(rightTrain), leftTest.union(rightTest))
+  })
+
+  finalResult._1.writeAsCsv(HOME_PDUY + "output_train.txt", "\n", "\t", WriteMode.OVERWRITE)
+  finalResult._2.writeAsCsv(HOME_PDUY + "output_test.txt", "\n", "\t", WriteMode.OVERWRITE)
+
 //    .print()
 
-  val end = System.nanoTime()
+  env.execute()
 
-  println("n epochs = " + iteration)
-  println("Elapsed Time = " + (end - begin)/1000000000 + "seconds")
+//  val end = System.nanoTime()
+//
+//  println("n epochs = " + iteration)
+//  println("Elapsed Time = " + (end - begin)/1000000000 + "seconds")
 }
